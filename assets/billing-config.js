8fbee75a4eb7801558dad5754e537b3167f4dde1
automly1-lab <automly1@gmail.com>
2026-04-29 01:11:23 +0300

    Wire Stripe sandbox payment links into billing config

--- assets/billing-config.js
+++ assets/billing-config.js
@@ -1,5 +1,5 @@
 window.RANKFORGE_BILLING = {
-  starterPaymentLink: "",
-  growthPaymentLink: "",
+  starterPaymentLink: "https://buy.stripe.com/test_00wfZh9s88AZ0uc6gx67S00",
+  growthPaymentLink: "https://buy.stripe.com/test_aFa7sL1ZG2cB90I7kB67S01",
   supportEmail: "support@crestlineops.com"
 };
